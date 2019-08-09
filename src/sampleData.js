d5da9fdbfa04adf2e54f7a7d52724acfc90ad6b4
manandearth <adamgefen@posteo.net>
2019-08-09 19:12:33 +0200

    Fix `sampleData` key

--- src/sampleData.js
+++ src/sampleData.js
@@ -3,13 +3,13 @@ const today = new Date();
 const at = hours => today.setHours(hours, 0);
 
 export const sampleAppointments = [
-	{startAt: at(9), customer: { firstName: 'Charlie' } },
-	{startAt: at(10), customer: { firstName: 'Frankie' } },
-	{startAt: at(11), customer: { firstName: 'Casey' } },
-	{startAt: at(12), customer: { firstName: 'Ashley' } },
-	{startAt: at(13), customer: { firstName: 'Jordan' } },
-	{startAt: at(14), customer: { firstName: 'Jay' } },
-	{startAt: at(15), customer: { firstName: 'Alex' } },
-	{startAt: at(16), customer: { firstName: 'Jules' } },
-	{startAt: at(17), customer: { firstName: 'Stevie' } }
+	{startsAt: at(9), customer: { firstName: 'Charlie' } },
+	{startsAt: at(10), customer: { firstName: 'Frankie' } },
+	{startsAt: at(11), customer: { firstName: 'Casey' } },
+	{startsAt: at(12), customer: { firstName: 'Ashley' } },
+	{startsAt: at(13), customer: { firstName: 'Jordan' } },
+	{startsAt: at(14), customer: { firstName: 'Jay' } },
+	{startsAt: at(15), customer: { firstName: 'Alex' } },
+	{startsAt: at(16), customer: { firstName: 'Jules' } },
+	{startsAt: at(17), customer: { firstName: 'Stevie' } }
 ];
