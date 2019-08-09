432981330d547c6da34e954d55260433bc470e07
manandearth <adamgefen@posteo.net>
2019-08-09 17:17:19 +0200

    Refactor tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -23,24 +23,30 @@ describe('Appointment', () => {
 
 describe('AppointmentsDayView', () => {
 	let container;
-
+  const render = component => ReactDOM.render(component, container);
+	const today = new Date();
+	const appointments = [
+		{ startsAt: today.setHours(12, 0) },
+		{ startsAt: today.setHours(13, 0) }];
 	beforeEach(() => {
 		container = document.createElement('div');
 	});
-
-	const render = component => ReactDOM.render(component, container);
+	
 	it('renders a div with the right id', () => {
 		render(<AppointmentsDayView appointments={[]} />);
 		expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
 	});
+	
 	it('renders multiple appointments in an ol element', () => {
-		const today = new Date();
-		const appointments = [
-			{ startsAt: today.setHours(12, 0) },
-			{ startsAt: today.setHours(13, 0) }];
 		render(<AppointmentsDayView appointments={ appointments } />);
 		expect(container.querySelector('ol')).not.toBeNull();
-		expect(
-			container.querySelector('ol').children).toHaveLength(2);
+		expect(container.querySelector('ol').children).toHaveLength(2);
 	});
+	
+	it('renders each appointment in an li', () => {
+		render(<AppointmentsDayView appointments={ appointments } />);
+		expect(container.querySelectorAll('li')).toHaveLength(2);
+		expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+		expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
 	});
+});
