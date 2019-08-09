118cfea8ec5a8d0056a85ab15222b755444dc4e1
manandearth <adamgefen@posteo.net>
2019-08-09 19:03:21 +0200

    Add sample data, entry point, test for button

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Appointment, AppointmentsDayView } from '../src/Appointment';
+import ReactTestUtils from 'react-dom/test-utils';
 
 describe('Appointment', () => {
 	let customer;
@@ -26,8 +27,8 @@ describe('AppointmentsDayView', () => {
   const render = component => ReactDOM.render(component, container);
 	const today = new Date();
 	const appointments = [
-		{ startsAt: today.setHours(12, 0) },
-		{ startsAt: today.setHours(13, 0) }];
+		{ startsAt: today.setHours(12, 0),  customer: {firstName: 'Ashley'} },
+		{ startsAt: today.setHours(13, 0),  customer: {firstName: 'Jordan'} }];
 	beforeEach(() => {
 		container = document.createElement('div');
 	});
@@ -49,4 +50,26 @@ describe('AppointmentsDayView', () => {
 		expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
 		expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
 	});
+
+  it('initially shows a message saying there are no appointments', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.textContent).toMatch('There are no appointments scheduled for today.'
+    );
+  });
+  it('selects the first appointment by default' , () => {
+  render(<AppointmentsDayView appointments={ appointments } />);
+    expect(container.textContent).toMatch('Ashley');
+  });
+  it('has a button element in every li', () => {
+    render(<AppointmentsDayView appointments={ appointments } />);
+    expect(container.querySelectorAll('li > button')).toHaveLength(2);
+    expect(container.querySelectorAll('li > button')[0].type).toEqual('button');
+  });
+  it('renders another appointment when selected', () => {
+    render(<AppointmentsDayView appointments={ appointments } />);
+    const button = container.querySelectorAll('button')[1];
+    ReactTestUtils.Simulate.click(button);
+    expect(container.textContent).toMatch('Jordan');
+    
+  });
 });
