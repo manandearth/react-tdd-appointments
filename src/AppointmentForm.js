793b0bfc5be87a56b5e19b134182f81cd476a7e1
manandearth <adamgefen@posteo.net>
2019-08-27 23:22:34 +0200

    Refactor `timeIncrenments`

--- src/AppointmentForm.js
+++ src/AppointmentForm.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
 
+const timeIncrement = (numTimes, startTime, increment) => Array(numTimes)
+  .fill([startTime])
+  .reduce((acc, _, i) => acc.concat([startTime + (i * increment)]));
+
 
 const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
   const totalSlots = (salonClosesAt - salonOpensAt) * 2;
   const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
   const increment = 30 * 60 * 1000;
-  return Array(totalSlots)
-    .fill([startTime])
-    .reduce((acc, _, i) => acc.concat([startTime + (i * increment)]));
+  return timeIncrement(totalSlots, startTime, increment);
 };
 
 const weeklyDateValues = (startDate) => {
   const midnight = new Date(startDate).setHours(0, 0, 0, 0);
   const increment = 24 * 60 * 60 * 1000;
-  return Array(7)
-    .fill([midnight])
-    .reduce((acc, _, i) => acc.concat([midnight + (i * increment)]));
+  return timeIncrement(7, midnight, increment);
 };
 
 const toTimeValue = (timestamp) => new Date(timestamp).toTimeString().substring(0, 5);
