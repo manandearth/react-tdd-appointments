54c3a059f1abc6f06159e075c5ff4cf050e3c29f
manandearth <adamgefen@posteo.net>
2019-08-29 15:02:39 +0200

    Update `src/sampleData` with example

--- src/sampleData.js
+++ src/sampleData.js
@@ -1,69 +1,74 @@
-const today = new Date();
+import { name, phone, lorem } from 'faker';
 
+Array.prototype.unique = function () {
+  return this.filter((value, index, self) => self.indexOf(value) === index);
+};
+
+Array.prototype.pickRandom = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+const today = new Date();
 const at = (hours) => today.setHours(hours, 0);
 
-export const sampleAppointments = [
-  {
-    startsAt: at(9),
-    customer: {
-      firstName: 'Charlie',
-      lastName: 'Nimrod',
-    },
-  },
-  {
-    startsAt: at(10),
-    customer: {
-      firstName: 'Frankie',
-      lastName: 'Kenneddy',
-    },
-  },
-	 {
-    startsAt: at(11),
-    customer: {
-      firstName: 'Casey',
-      lastName: 'Chaplin',
-    },
-  },
-  {
-    startsAt: at(12),
-    customer: {
-      firstName: 'Ashley',
-      lastName: 'Smith',
-    },
-  },
-  {
-    startsAt: at(13),
-    customer: {
-      firstName: 'Jordan',
-      lastName: 'Air',
-    },
-  },
-  {
-    startsAt: at(14),
-    customer: {
-      firstName: 'Jay',
-      lastName: 'Lohr',
-    },
-  },
-  {
-    startsAt: at(15),
-    customer: {
-      firstName: 'Alex',
-      lastName: 'Knight',
-    },
-  },
-  {
-    startsAt: at(16),
-    customer: {
-      firstName: 'Jules',
-      lastName: 'Joplin',
-    },
-  },
-  {
-    startsAt: at(17),
-    customer: {
-      firstName: 'Stevie',
-      lastName: 'Ray Vaughn',
-    },
-  },
+const stylists = [0, 1, 2, 3, 4, 5, 6]
+  .map(() => name.firstName())
+  .unique();
+
+const services = [
+  'Cut',
+  'Blow-dry',
+  'Cut & color',
+  'Beard trim',
+  'Cut & beard trim',
+  'Extensions',
 ];
+
+const generateFakeCustomer = () => ({
+  firstName: name.firstName(),
+  lastName: name.lastName(),
+  phoneNumber: phone.phoneNumberFormat(1),
+});
+
+const generateFakeAppointment = () => ({
+  customer: generateFakeCustomer(),
+  stylist: stylists.pickRandom(),
+  service: services.pickRandom(),
+  notes: lorem.paragraph(),
+});
+
+export const sampleAppointments = [
+  { startsAt: at(9), ...generateFakeAppointment() },
+  { startsAt: at(10), ...generateFakeAppointment() },
+  { startsAt: at(11), ...generateFakeAppointment() },
+  { startsAt: at(12), ...generateFakeAppointment() },
+  { startsAt: at(13), ...generateFakeAppointment() },
+  { startsAt: at(14), ...generateFakeAppointment() },
+  { startsAt: at(15), ...generateFakeAppointment() },
+  { startsAt: at(16), ...generateFakeAppointment() },
+  { startsAt: at(17), ...generateFakeAppointment() },
+];
+
+const pickMany = (items, number) => Array(number)
+  .fill(1)
+  .map(() => items.pickRandom());
+
+const buildTimeSlots = () => {
+  const today = new Date();
+  const startTime = today.setHours(9, 0, 0, 0);
+  const times = [...Array(7).keys()].map((day) => {
+    const daysToAdd = day * 24 * 60 * 60 * 1000;
+    return [...Array(20).keys()].map((halfHour) => {
+      const halfHoursToAdd = halfHour * 30 * 60 * 1000;
+      return {
+        startsAt: startTime + daysToAdd + halfHoursToAdd,
+      };
+    });
+  });
+  return [].concat(...times);
+};
+
+export const sampleAvailableTimeSlots = pickMany(
+  buildTimeSlots(),
+  50
+);
